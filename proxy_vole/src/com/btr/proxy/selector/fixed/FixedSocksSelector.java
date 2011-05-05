13870e9bfb84863924b341272701a871150da6d8
rosstauscher <rosstauscher@gmx.de>
2011-05-05 20:20:05 +0000

    First support for OSX implemented

--- proxy_vole/src/com/btr/proxy/selector/fixed/FixedSocksSelector.java
+++ proxy_vole/src/com/btr/proxy/selector/fixed/FixedSocksSelector.java
@@ -1,14 +1,7 @@
 package com.btr.proxy.selector.fixed;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
-import java.net.ProxySelector;
-import java.net.SocketAddress;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /*****************************************************************************
  * This proxy selector is configured with a fixed proxy. This proxy will be 
@@ -18,9 +11,7 @@ import java.util.List;
  * @author Bernd Rosstauscher (proxyvole@rosstauscher.de) Copyright 2009
  ****************************************************************************/
 
-public class FixedSocksSelector extends ProxySelector {
-	
-	private final List<Proxy> proxyList;
+public class FixedSocksSelector extends FixedProxySelector {
 	
 	/*************************************************************************
 	 * Constructor
@@ -29,33 +20,8 @@ public class FixedSocksSelector extends ProxySelector {
 	 ************************************************************************/
 	
 	public FixedSocksSelector(String proxyHost, int proxyPort) {
-		super();
-		Proxy proxy = new Proxy(Proxy.Type.SOCKS, 
-				InetSocketAddress.createUnresolved(proxyHost, proxyPort));
-
-		List<Proxy> list = new ArrayList<Proxy>(1);
-		list.add(proxy);
-		this.proxyList = Collections.unmodifiableList(list);
-	}
-
-	/*************************************************************************
-	 * connectFailed
-	 * @see java.net.ProxySelector#connectFailed(java.net.URI, java.net.SocketAddress, java.io.IOException)
-	 ************************************************************************/
-
-	@Override
-	public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
-		// Not used
-	}
-
-	/*************************************************************************
-	 * select
-	 * @see java.net.ProxySelector#select(java.net.URI)
-	 ************************************************************************/
-
-	@Override
-	public List<Proxy> select(URI uri) {
-		return this.proxyList;
+		super(new Proxy(Proxy.Type.SOCKS, 
+				InetSocketAddress.createUnresolved(proxyHost, proxyPort)));
 	}
 
 }
