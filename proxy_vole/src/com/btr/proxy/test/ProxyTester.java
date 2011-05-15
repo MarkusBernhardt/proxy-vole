8842439ea6afe083b38f3dc76ed5c1bb90726cca
rosstauscher <rosstauscher@gmx.de>
2011-05-15 18:10:04 +0000

    Added logging area

--- proxy_vole/src/com/btr/proxy/test/ProxyTester.java
+++ proxy_vole/src/com/btr/proxy/test/ProxyTester.java
@@ -1,10 +1,12 @@
 package com.btr.proxy.test;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.Proxy;
 import java.net.ProxySelector;
 import java.net.URL;
+import java.text.MessageFormat;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -13,11 +15,15 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import com.btr.proxy.search.ProxySearch;
 import com.btr.proxy.search.ProxySearch.Strategy;
+import com.btr.proxy.util.Logger;
+import com.btr.proxy.util.Logger.LogLevel;
 
 /*****************************************************************************
  * Small test application that allows you to select a proxy search strategy
@@ -32,6 +38,8 @@ public class ProxyTester extends JFrame {
 	private JComboBox modes;
 	private JButton testButton;
 	private JTextField urlField;
+
+	private JTextArea logArea;
 
 	/*************************************************************************
 	 * Constructor
@@ -51,7 +59,6 @@ public class ProxyTester extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel p = new JPanel();
-		setContentPane(p);
 
 		p.add(new JLabel("Mode:"));
 
@@ -71,8 +78,32 @@ public class ProxyTester extends JFrame {
 		});
 		p.add(this.testButton);
 
+		this.logArea = new JTextArea(5, 50);
+		JPanel contenPane = new JPanel(new BorderLayout());
+		contenPane.add(p, BorderLayout.NORTH);
+		contenPane.add(new JScrollPane(this.logArea), BorderLayout.CENTER);
+		setContentPane(contenPane);
+
 		pack();
 		setLocationRelativeTo(null);
+		installLogger();
+	}
+
+	/*************************************************************************
+	 * Install the framework logger.
+	 ************************************************************************/
+	
+	private void installLogger() {
+		Logger.setBackend(new Logger.LogBackEnd() {
+			@Override
+			public void log(Class<?> clazz, LogLevel loglevel, String msg, Object... params) {
+				ProxyTester.this.logArea.append(loglevel+"\t"+MessageFormat.format(msg, params)+"\n");
+			}
+			@Override
+			public boolean isLogginEnabled(LogLevel logLevel) {
+				return true;
+			}
+		});
 	}
 
 	/*************************************************************************
@@ -85,6 +116,8 @@ public class ProxyTester extends JFrame {
 				JOptionPane.showMessageDialog(this, "Please enter an URL first.");
 				return;
 			}
+			
+			this.logArea.setText("");
 
 			Strategy pss = (Strategy) this.modes.getSelectedItem();
 			ProxySearch ps = new ProxySearch();
