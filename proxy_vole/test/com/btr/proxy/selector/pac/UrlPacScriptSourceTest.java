14aa4d0623408793576a1583b24c3a2cac69c2bc
rosstauscher <rosstauscher@googlemail.com>
2013-12-03 12:13:15 +0000

    Issue 39 : Set smaller default timeouts and allow to specify them via system property.

--- proxy_vole/test/com/btr/proxy/selector/pac/UrlPacScriptSourceTest.java
+++ proxy_vole/test/com/btr/proxy/selector/pac/UrlPacScriptSourceTest.java
@@ -29,6 +29,30 @@ public class UrlPacScriptSourceTest {
 		String charset = scriptSource.parseCharsetFromHeader("application/octet-stream;");
 		assertEquals("ISO-8859-1", charset);
 	}
+	
+	/*************************************************************************
+	 * Unit Test
+	 ************************************************************************/
+	@Test
+	public void overrideTimeoutShouldWork() {
+		System.setProperty(UrlPacScriptSource.OVERRIDE_CONNECT_TIMEOUT, "5000");
+		UrlPacScriptSource scriptSource = new UrlPacScriptSource("");
+		int timeout = scriptSource.getTimeOut(UrlPacScriptSource.OVERRIDE_CONNECT_TIMEOUT, 1000);
+		System.clearProperty(UrlPacScriptSource.OVERRIDE_CONNECT_TIMEOUT);
+		assertEquals(5000, timeout);
+	}
+	
+	/*************************************************************************
+	 * Unit Test
+	 ************************************************************************/
+	@Test
+	public void timeoutShouldUseDefault() {
+		System.setProperty(UrlPacScriptSource.OVERRIDE_CONNECT_TIMEOUT, "XXX");
+		UrlPacScriptSource scriptSource = new UrlPacScriptSource("");
+		int timeout = scriptSource.getTimeOut(UrlPacScriptSource.OVERRIDE_CONNECT_TIMEOUT, 1000);
+		System.clearProperty(UrlPacScriptSource.OVERRIDE_CONNECT_TIMEOUT);
+		assertEquals(1000, timeout);
+	}
 
 }
 
