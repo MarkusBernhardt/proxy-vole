83f41deb2557db36ebb7d199f9ff4042daa5d33c
Markus Bernhardt <markus.bernhardt@me.com>
2016-05-16 01:10:39 +0200

    Moved tests

---
+++ src/test/java/com/github/markusbernhardt/autoproxy/selector/fixed/FixedProxyTest.java
@@ -0,0 +1,53 @@
+package com.github.markusbernhardt.autoproxy.selector.fixed;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.github.markusbernhardt.autoproxy.TestUtil;
+
+/*****************************************************************************
+ * 
+ * @author Bernd Rosstauscher (proxyvole@rosstauscher.de) Copyright 2009
+ ****************************************************************************/
+
+public class FixedProxyTest {
+
+    /*************************************************************************
+     * Test method
+     ************************************************************************/
+    @Test
+    public void testFixedProxy() {
+        ProxySelector ps = new FixedProxySelector("http_proxy.unit-test.invalid", 8090);
+
+        List<Proxy> result = ps.select(TestUtil.HTTP_TEST_URI);
+        assertEquals(TestUtil.HTTP_TEST_PROXY, result.get(0));
+    }
+
+    /*************************************************************************
+     * Test method
+     ************************************************************************/
+    @Test
+    public void testFixedProxy2() {
+        ProxySelector ps = new FixedProxySelector(TestUtil.HTTP_TEST_PROXY);
+
+        List<Proxy> result = ps.select(TestUtil.HTTP_TEST_URI);
+        assertEquals(TestUtil.HTTP_TEST_PROXY, result.get(0));
+    }
+
+    /*************************************************************************
+     * Test method
+     ************************************************************************/
+    @Test
+    public void testFixedProxy3() {
+        ProxySelector ps = new FixedProxySelector(TestUtil.HTTP_TEST_PROXY);
+
+        List<Proxy> result = ps.select(TestUtil.HTTPS_TEST_URI);
+        assertEquals(TestUtil.HTTP_TEST_PROXY, result.get(0));
+    }
+
+}
